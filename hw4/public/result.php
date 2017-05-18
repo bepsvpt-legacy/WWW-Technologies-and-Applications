edcfcc478aabd1d9d924880ecae68aabff3b2766
BePsvPT <BePsvPT@users.noreply.github.com>
2017-05-18 13:49:18 +0800

    hw4 fix database connection error
    Reference: https://stackoverflow.com/a/22927341

--- hw4/public/result.php
+++ hw4/public/result.php
@@ -12,7 +12,7 @@ $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => 'localhost',
+    'host'      => '127.0.0.1',
     'database'  => getenv('DB_DATABASE'),
     'username'  => getenv('DB_USERNAME'),
     'password'  => getenv('DB_PASSWORD'),
